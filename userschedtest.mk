69d8668ee2f8b6d808c81b64600b4264818088db
Aeneas Meier <aeneas.meier@stud.unibas.ch>
2025-04-04 10:21:05 +0000

    add users

--- userschedtest.mk
+++ userschedtest.mk
@@ -10,4 +10,10 @@ define USERSCHEDTEST_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/userschedtest $(TARGET_DIR)/usr/bin/userschedtest
 endef
 
+define USERSCHEDTEST_USERS
+    $(INSTALL_DATA) $(@D)/users_table.txt $(TARGET_DIR)/etc/users_table
+endef
+
+USERSCHEDTEST_POST_INSTALL_TARGET_HOOKS += USERSCHEDTEST_USERS
+
 $(eval $(generic-package))
