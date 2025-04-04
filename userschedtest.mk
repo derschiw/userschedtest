149d15c32e5c6411516f7deb8ac03c429b5741bf
Aeneas Meier <aeneas.meier@stud.unibas.ch>
2025-04-04 10:54:43 +0000

    replace in favour of new users

--- userschedtest.mk
+++ userschedtest.mk
@@ -11,7 +11,9 @@ define USERSCHEDTEST_INSTALL_TARGET_CMDS
 endef
 
 define USERSCHEDTEST_USERS
-    user -1 userschedtest -1 * - - - Testuser 1
+    user1 -1 userschedtest -1 = /home/user1 /bin/sh - Test User 1
+    user2 -1 userschedtest -1 = /home/user2 /bin/sh - Test User 2
+    user3 -1 userschedtest -1 = /home/user3 /bin/sh - Test User 3   
 endef
 
 $(eval $(generic-package))
