d834c5f4f62860a9a1b7c8d67d3999c50c4f78cf
Aeneas Meier <aeneas.meier@stud.unibas.ch>
2025-05-30 09:15:25 +0000

    update tasks

--- userschedrun.c
+++ userschedrun.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 
 int main() {
-    system("userschedtest 7 &");
-    system("userschedtest 7 &");
+    system("su -u root  userschedtest 7 &");
+    system("su -u user1 userschedtest 7 &");
+    system("su -u user2 userschedtest 7 &");
+    system("su -u user3 userschedtest 7 &");
     return 0;
 }
