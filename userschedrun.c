6e4a4ba832784d99763d48e5bda2caae0ff0ae94
Aeneas Meier <aeneas.meier@stud.unibas.ch>
2025-05-30 09:49:06 +0000

    update dummy commands

--- userschedrun.c
+++ userschedrun.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 
 int main() {
-    system("su - root  -c '/usr/bin/userschedtest 7 &'");
-    system("su - user1 -c '/usr/bin/userschedtest 7 &'");
-    system("su - user2 -c '/usr/bin/userschedtest 7 &'");
-    system("su - user3 -c '/usr/bin/userschedtest 7 &'");
+    system("su - root  -c 'chpol 7 yes > /dev/null &'");
+    system("su - user1 -c 'chpol 7 yes > /dev/null &'");
+    system("su - user2 -c 'chpol 7 yes > /dev/null &'");
+    system("su - user3 -c 'chpol 7 yes > /dev/null &'");
     return 0;
 }
