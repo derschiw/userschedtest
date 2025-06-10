11ae065f5bc4fdf5a3fbc38bf51aeea3960bd394
Aeneas Meier <aeneas.meier@stud.unibas.ch>
2025-06-10 08:02:18 +0000

    change color

--- userschedrun.c
+++ userschedrun.c
@@ -617,7 +617,7 @@ void demo1(){
         pid_t pid_user_1_1 = fork();
         if (pid_user_1_1 == 0) {
             char cmd[256];
-            snprintf(cmd, sizeof(cmd), "head -c 2000000 </dev/urandom | sha256sum > /dev/null");
+            snprintf(cmd, sizeof(cmd), "head -c 2000001 </dev/urandom | sha256sum > /dev/null");
             __measure("user1", cmd, &i, 7, 2, 1,0);
             exit(EXIT_SUCCESS);
         } else if (pid_user_1_1 < 0) {
